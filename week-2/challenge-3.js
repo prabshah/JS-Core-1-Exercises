c638b44cac1fbae7b2e0f1ab90b5cdcbe0d2167f
prabshah <prabesh.shah31@gmail.com>
2017-11-06 10:11:40 +0000

    JS-Core-1-Exercise/week-2/challenge-2

--- week-2/challenge-3.js
+++ week-2/challenge-3.js
@@ -14,3 +14,54 @@ var myArray = [
   [ 8, 9, 12, 15],
   [ 20, 30, 50, 72, 81]
 ];
+
+const combinedArr = (arr) => {
+  var result = [];
+  for (var i = 0; i <= arr.length -1; i++) {
+   for (j = 0; j <= arr[i].length - 1; j++) {
+     result.push(arr[i][j]);
+   }
+  }
+  return result;
+}
+
+console.log(combinedArr(myArray));
+
+// trying out the function combinedArr using a very long unsorted array
+var largeArray = [[12, 35, 2, 0, 67, 65], [12, 78, 23, 1, 3, 73, 98],
+[23, 46, 87, 89, 97, 34], [0, 6, 87, 45, 66, 77, 88], [12, 13, 14, 15],
+[11, 21, 32, 43, 54]]
+
+// storing the result of combinedArr function call in a variable
+var combinedArray = combinedArr(largeArray);
+
+console.log(combinedArray);
+
+// sorting the reslut using sort()
+
+//combinedArray.sort(function(x, y) {
+// return x - y;
+//});
+
+
+// sorting the result without using sort()
+console.log('sorting the result without using sort()');
+
+
+var temp;
+for (var i = 0; i <= combinedArray.length - 1; i++) {
+  
+  for (var j = 0; j <= combinedArray.length - 1; j++) {
+    if (combinedArray[j] > combinedArray[j + 1]) {
+      temp = combinedArray[j + 1];
+      combinedArray[j + 1] = combinedArray[j];
+      combinedArray[j] = temp;
+    }
+
+  } 
+  
+}
+console.log(combinedArray);
+
+
+
